8f66134ac8790145c33d6a361f1d438518dfc22d
Lily Wang <lily@minium.com.au>
2020-06-14 14:22:53 +1000

    updated versions url

--- package/doc/sphinx/source/conf.py
+++ package/doc/sphinx/source/conf.py
@@ -158,7 +158,7 @@ html_theme_options = {
 }
 
 html_context = {
-    'versions_json_url': 'https://lilyminium.github.io/mdanalysis/versions.json'
+    'versions_json_url': 'https://mdadocs.minium.com.au/versions.json'
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
